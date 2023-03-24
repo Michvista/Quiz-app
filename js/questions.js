23e1c069ca053ea5e58cc52bb7bfdb48f05f6453
Mohammedtemitope <128607146+Mohammedtemitope@users.noreply.github.com>
2023-03-24 11:34:36 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -441,5 +441,109 @@ let questions = [
      ]
 
    },
-];
+{
 
+     numb: 22,
+
+     question: "Your Question is Here",
+
+     answer: "Correct answer of the  is here",
+
+     options: [
+
+       "Option 1",
+
+       "option 5",
+
+       "option 3",
+
+       "option 7"
+
+     ]
+
+   },
+{
+
+     numb: 22,
+
+     question: "Your Question is Here",
+
+     answer: "Correct answer of the  is here",
+
+     options: [
+
+       "Option 1",
+
+       "option 5",
+
+       "option 3",
+
+       "option 7"
+
+     ]
+
+   },
+{
+
+     numb: 22,
+
+     question: "Your Question is Here",
+
+     answer: "Correct answer of the  is here",
+
+     options: [
+
+       "Option 1",
+
+       "option 5",
+
+       "option 3",
+
+       "option 7"
+
+     ]
+
+   },
+ {
+
+     numb: 22,
+
+     question: "Your Question is Here",
+
+     answer: "Correct answer of the  is here",
+
+     options: [
+
+       "Option 1",
+
+       "option 5",
+
+       "option 3",
+
+       "option 7"
+
+     ]
+
+   },
+    {
+
+     numb: 22,
+
+     question: "Your Question is Here",
+
+     answer: "Correct answer of the  is here",
+
+     options: [
+
+       "Option 1",
+
+       "option 5",
+
+       "option 3",
+
+       "option 7"
+
+     ]
+
+   },
+ ];
