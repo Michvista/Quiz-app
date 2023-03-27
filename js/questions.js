9bbb0a618e766346c890021cc691c9719f7efa5b
Michvista <99558272+Michvista@users.noreply.github.com>
2023-03-27 03:50:59 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,56 +1,1010 @@
-let questions = [ 
-     { 
-     numb: 1, 
-     question: "What does HTML stand for?", 
-     answer: "Hyper Text Markup Language", 
-     options: [ 
-       "Hyper Text Preprocessor", 
-       "Hyper Text Markup Language", 
-       "Hyper Text Multiple Language", 
-       "Hyper Tool Multi Language" 
-     ] 
-   }, 
-     { 
-     numb: 2, 
-     question: "What does CSS stand for?", 
-     answer: "Cascading Style Sheet", 
-     options: [ 
-       "Common Style Sheet", 
-       "Colorful Style Sheet", 
-       "Computer Style Sheet", 
-       "Cascading Style Sheet" 
-     ] 
-   }, 
-     { 
-     numb: 3, 
-     question: "What does PHP stand for?", 
-     answer: "Hypertext Preprocessor", 
-     options: [ 
-       "Hypertext Preprocessor", 
-       "Hypertext Programming", 
-       "Hypertext Preprogramming", 
-       "Hometext Preprocessor" 
-     ] 
-   }, 
-     { 
-     numb: 4, 
-     question: "What does SQL stand for?", 
-     answer: "Structured Query Language", 
-     options: [ 
-       "Stylish Question Language", 
-       "Stylesheet Query Language", 
-       "Statement Question Language", 
-       "Structured Query Language" 
-     ] 
-   }, 
-     { 
-     numb: 5, 
-     question: "What does XML stand for?", 
-     answer: "eXtensible Markup Language", 
-     options: [ 
-       "eXtensible Markup Language", 
-       "eXecutable Multiple Language", 
-       "eXTra Multi-Program Language", 
-       "eXamine Multiple Language" 
-     ] 
-   },
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A force which Balance out the sum of other forces is ?",
+    answer: "Equilibrant",
+    options: [
+      "Resultant",
+      "Parallel force",
+      "Equilibrant",
+      "Component force"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What displacement must be added to a 50cm displacement in the +x direction to give a resultant displacement of 85cm at 25degree",
+    answer: "45cm",
+    options: [
+      "12cm",
+      "18.8cm",
+      "138.8cm",
+      "45cm"
+    ]
+  },
+    {
+    numb: 3,
+    question: "With an Airspeed of 100km/hr a pilot Flies a course bearing of 120degree if a steady wind of 50km/hr is blowing from a bearing of 080degree Determine the ground speed",
+    answer: "69.6km/hr",
+    options: [
+      "69.6km/hr",
+      "32.0km/hr",
+      "19.0km/hr",
+      "120.78km/hr"
+    ]
+  },
+    {
+    numb: 4,
+    question: "which of the following type of motion is produced by a couple",
+    answer: "Rotational",
+    options: [
+      "Oscillatory",
+      "Rotational",
+      "Random",
+      "Translational"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A force 10N and 50N act at an angle 60degree to each other. Find the magnitude of the Equilibrant",
+    answer: "13.2units",
+    options: [
+      "10.0units",
+      "26.4units",
+      "15.0units",
+      "13.2units"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+     numb: 6,
+     question: "The point in which the whole weight of a body concentrate is known as?",
+     answer: "Center of gravity",
+     options: [
+       "middle point",
+       "Maximum point",
+       "Centre of gravity",
+       "Equilibrium point"
+     ]
+   },
+    {
+
+     numb: 7,
+
+     question: "A body moving on a smooth inclined plane will experience?",
+
+     answer: "Zero Friction",
+
+     options: [
+
+       "Maximum friction",
+
+       "Retardation",
+
+       "Zero Friction",
+
+       "minimum acceleration"
+
+     ]
+
+   },
+
+{
+
+     numb: 8,
+
+     question: "A 5.0 long uniform plank is supported at the centre of gravity . A 3.0kg boy sits at one end of the plank. A second boy sit 0.3m from the other end calculate the boy weight if the plank is in equilibrium horizontally",
+
+     answer: "340.9N",
+
+     options: [
+
+       "6932N",
+
+       "340.9N",
+
+       "25.17N",
+
+       "39.16N"
+
+     ]
+
+   },
+
+{
+
+     numb: 9,
+
+     question: "The sum of all forces under Equilibrium is?",
+
+     answer: "0",
+
+     options: [
+
+       "Equal",
+
+       "Maximum",
+
+       "0",
+
+       "Minimum"
+
+     ]
+
+   },
+
+{
+
+     numb: 10,
+
+     question: "A boat heads 20degree west of North across a canal at a speed of 12m/s what is the resultsnt velocity of the boat if water in the cannal flow due east 12m/s",
+
+     answer: "13.76m/s",
+
+     options: [
+
+       "6.66m/s",
+
+       "27.7m/s",
+
+       "13.76m/s",
+
+       "34.4m/s"
+
+     ]
+
+   },
+
+{
+
+     numb: 11,
+
+     question: "A long jumper had take off velocity of 9.5m/s What is the maximum horizontal distance?",
+
+     answer: "9.21m",
+
+     options: [
+
+       "19.45m",
+
+       "12.66m",
+
+       "9.21m",
+
+       "19.45m"
+
+     ]
+
+   },
+
+{
+
+     numb: 12,
+
+     question: "A body thrown with v=5i+8j in m/s find the time taken to reach maximum height",
+
+     answer: "0.825",
+
+     options: [
+
+       "0.825s",
+
+       "44.5s",
+
+       "0.34s",
+
+       "23.3s"
+
+     ]
+
+   },
+{
+
+     numb: 13,
+
+     question: "Express the speed of a car travelling at 80km/hr in mm/s",
+
+     answer: "2.22×10^4mm/s",
+
+     options: [
+
+       "2.22x10^3mm/s",
+
+       "22.2×10^5mm/s",
+
+       "2.22×10^-3mm/s",
+
+       "2.22×10^4mm/s"
+
+     ]
+
+   },
+{
+
+     numb: 14,
+
+     question: "vector a=-i-4j+2k b=3i+2j-2k c=2i-3j+k calculate a.(b×c)",
+
+     answer: "6",
+
+     options: [
+
+       "-6",
+
+       "3",
+
+       "6",
+
+       "12"
+
+     ]
+
+   },
+
+{
+
+     numb: 15,
+
+     question: "Two students carry a 260N suitcase by two straps which make angle 22deegree and 30degree respectively with the vertical calculate the force applied by each student to keep the suitcase in equilibrium",
+
+     answer: "123.6,165.0",
+
+     options: [
+
+       "1.72,39.9",
+
+       "123.6,165.0",
+
+       "26.6,57.2",
+
+       "123.6,1.72"
+
+     ]
+
+   },
+
+{
+
+     numb: 16,
+
+     question: "vector A,B,C has an equilibrant of of?",
+
+     answer: "A+B+C",
+
+     options: [
+
+       "-(A+B+C)",
+
+       "A+B+C",
+
+       "A-B-C",
+
+       "-(B-C)+B"
+
+     ]
+
+   },
+
+{
+
+     numb: 17,
+
+     question: "A particle projected with a velocity of 40m/s at an elevation of 60° calculate the Vertical Component of velocity at height of 50m",
+
+     answer: "2√25m/s",
+
+     options: [
+
+       "4√115m/s",
+
+       "25√3m/s",
+
+       "2√545m/s",
+
+       "2√25m/s"
+
+     ]
+
+   },
+
+{
+
+     numb: 18,
+
+     question: "A single force which produce the same effect as a set of forces acting together at a point is known as the ?",
+
+     answer: "Resultant",
+
+     options: [
+
+       "component",
+
+       "Equilibrant",
+
+       "Resistant",
+
+       "Resultant"
+
+     ]
+
+   },
+
+{
+
+     numb: 19,
+
+     question: "An example of scalar quantity",
+
+     answer: "Electric charge",
+
+     options: [
+
+       "velocity",
+
+       "Electric charge",
+
+       "Momentum",
+
+       "Weight"
+
+     ]
+
+   },
+
+{
+
+     numb: 20,
+
+     question: "The resultant of two Forces acting on an object is Maximum when the angle between them is ?",
+
+     answer: "0°",
+
+     options: [
+
+       "180°",
+
+       "90°",
+
+       "45°",
+
+       "0°"
+
+     ]
+
+   },
+
+{
+
+     numb: 21,
+
+     question: "During inelastic collision",
+
+     answer: "Only momentum is conserved ",
+
+     options: [
+
+       "momentum and energy conserved",
+
+       "Only momentum is conserved",
+
+       "Momentum and energy not conserved",
+
+       "Momentum and energy increases"
+
+     ]
+
+   },
+{
+
+     numb: 22,
+
+     question: "A 65kg sprinter completes a 100m race in 9.83s calculate the average kinetic energy of the sprinter",
+
+     answer: "3.36kJ",
+
+     options: [
+
+       "26.3kJ",
+
+       "45.2kJ",
+
+       "3.36kJ",
+
+       "20.0kJ"
+
+     ]
+
+   },
+{
+
+     numb: 23,
+
+     question: "If a plane is beign Rotated through an angle of 30° above the Horizontal the Reaction will rotate through?",
+
+     answer: "30°",
+
+     options: [
+
+       "150°",
+
+       "30°",
+
+       "60°",
+
+       "120°
+
+     ]
+
+   },
+{
+
+     numb: 24,
+
+     question: "what course is",
+
+     answer: "Physics",
+
+     options: [
+
+       "chemistry",
+
+       "Physics",
+
+       "Botany",
+
+       "Zoology"
+
+     ]
+
+   },
+{
+
+     numb: 25,
+
+     question: "The reaction a body is always ? to the plane",
+
+     answer: "Perpendicular",
+
+     options: [
+
+       "Perpendicular",
+
+       "Linear",
+
+       "Co-linear",
+
+       "Parallel"
+
+     ]
+
+   },
+{
+
+     numb: 26,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+ {
+
+     numb: 27,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 28,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+ {
+
+     numb: 29,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 30,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 31,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 32,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 33,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 34,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 35,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 36,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 37,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 38,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 39,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 40,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 41,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 42,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 43,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 44,
+
+     question: "",
+
+     answer: "",
+      
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 45,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 46,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 47,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 48,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 49,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    {
+
+     numb: 50,
+
+     question: "",
+
+     answer: "",
+
+     options: [
+
+       "",
+
+       "",
+
+       "",
+
+       ""
+
+     ]
+
+   },
+    
+];
